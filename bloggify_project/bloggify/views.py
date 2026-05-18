3b628c7ada5b46b24bc1db0a1bc93dcb1b7b0393
ilia <roozbanilia@gmail.com>
2026-05-18 20:37:08 +0330

    added some vies and tampleates

--- bloggify_project/bloggify/views.py
+++ bloggify_project/bloggify/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from typing import Any
 
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from django.views import generic
+from . models import *
 # Create your views here.
+
+class ListCategoryView(generic.ListView):
+    context_object_name  = 'categories'
+    model = Category
+    
+
+class ListPostView(generic.ListView):
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-pub_date')
+    
+
+class ListcommentView(generic.ListView):
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Comment.objects.order_by('-pub_date')
